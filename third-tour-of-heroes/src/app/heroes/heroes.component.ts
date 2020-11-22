a021e932822a937182b20d77748d1efcf3bfe4cc
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-11-22 15:14:32 -0800

    Created HEROES list

--- third-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ third-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +9,7 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   hero: Hero = {
     name: 'MeowMan',
     id: 1

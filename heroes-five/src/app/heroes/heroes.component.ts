837d872c9e6c502d8b57d51694581e176b890ec0
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-11-24 15:51:51 -0800

    Imported observable/of to hero service and changed heroes component getHeroes method to be async

--- heroes-five/src/app/heroes/heroes.component.ts
+++ heroes-five/src/app/heroes/heroes.component.ts
@@ -15,10 +15,15 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }

a57ec38467548c2c1eb867e153e0ef6404bddaf1
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-11-24 20:13:23 -0800

    Added hero component

--- heroes-five/src/app/heroes/heroes.component.ts
+++ heroes-five/src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +10,24 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
+
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[];
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
     this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+        .subscribe(heroes => this.heroes = heroes);
   }
 }

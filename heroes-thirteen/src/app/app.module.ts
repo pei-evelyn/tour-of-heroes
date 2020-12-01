814abf11e54c287c8548affb7006147ab549c8e6
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-01 14:31:45 -0800

    Added step 3

--- heroes-thirteen/src/app/app.module.ts
+++ heroes-thirteen/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HerosComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,

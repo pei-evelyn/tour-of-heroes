fda173bc58c34964b557bc89e3560fce5e4c7414
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-01 14:22:18 -0800

    Added steps 1-2

---
+++ heroes-thirteen/src/app/heros/heros.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}

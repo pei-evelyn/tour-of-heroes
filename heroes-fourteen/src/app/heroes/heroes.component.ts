72f6d7dfc0b8a291298f7cb00774dfb9b417ac17
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-02 15:05:31 -0800

    Removed message component from heroes

--- heroes-fourteen/src/app/heroes/heroes.component.ts
+++ heroes-fourteen/src/app/heroes/heroes.component.ts
@@ -1,4 +1,3 @@
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroesService } from '../heroes.service';

8eafed0c2062b1435372b8f16045ae02db7867cc
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-01 16:57:16 -0800

    Completed step 5

--- heroes-thirteen/src/app/dashboard/dashboard.component.ts
+++ heroes-thirteen/src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,12 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heros',
-  templateUrl: './heros.component.html',
-  styleUrls: ['./heros.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class HerosComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
@@ -19,6 +18,6 @@ export class HerosComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }

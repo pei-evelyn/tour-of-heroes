531d2b0d8fad292f8c955f83a66abad37c6cdb8b
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-03 15:29:14 -0800

    Added routing to hero-detail

--- heroes-fifteen/src/app/hero.service.ts
+++ heroes-fifteen/src/app/hero.service.ts
@@ -16,4 +16,9 @@ export class HeroService {
     this.messageService.add('HeroService: HEROES fetched');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: Hero Id ${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }

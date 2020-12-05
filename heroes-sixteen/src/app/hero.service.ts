72d72028e7563a4d71de230ab1631bc38844d115
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-04 21:02:10 -0800

    Added pipe to http req

--- heroes-sixteen/src/app/hero.service.ts
+++ heroes-sixteen/src/app/hero.service.ts
@@ -22,6 +22,7 @@ export class HeroService {
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
+      console.error(error);
       this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     };
@@ -30,11 +31,17 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(_ => this.log('fetched heroes')),
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
 
   getHero(id: number): Observable<Hero> {
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+    .pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 }

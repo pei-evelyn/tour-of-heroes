4482d0c2285c71c5bb6eb47f97f828c3a9d9a145
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-04 16:28:39 -0800

    Added http client to get from api

--- heroes-sixteen/src/app/hero.service.ts
+++ heroes-sixteen/src/app/hero.service.ts
@@ -3,21 +3,38 @@ import { Observable, of } from 'rxjs';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('Heroes Fetcheddddd');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`Hero fetched id ${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
 }
